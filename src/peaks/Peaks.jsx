f4f214a6ce8f39cfbdb1bf13edb7037c1f2e59e6
Theo Paaske <bwark@redwood.local>
2015-11-25 13:53:56 -0800

    yosemite terrain

--- src/peaks/Peaks.jsx
+++ src/peaks/Peaks.jsx
@@ -1,66 +1,57 @@
 import React from 'react';
 import { Button, ButtonGroup } from 'react-bootstrap';
 import THREE from 'three';
+import yos from './elevations.json';
 
 const Component = React.createClass({
     componentDidMount: function() {
-        // create a scene, that will hold all our elements such as objects, cameras and lights.
         var scene = new THREE.Scene();
+        // scene.fog = new THREE.FogExp2(0xffffff, 0.015);
+        scene.fog = new THREE.Fog(0xffffff, 10, 200);
 
-        // create a camera, which defines where we're looking at.
-        var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-        // create a render and set the size
         var renderer = new THREE.WebGLRenderer();
-
-        renderer.setClearColor(new THREE.Color(0xEEEEEE, 1.0));
+        renderer.setClearColor(new THREE.Color(0xffffff, 1.0));
         renderer.setSize(window.innerWidth, window.innerHeight);
-        renderer.shadowMapEnabled = true;
 
-        // show axes in the screen
-        // var axes = new THREE.AxisHelper(20);
-        // scene.add(axes);
+        var axes = new THREE.AxisHelper(20);
+        scene.add(axes);
 
-        // position and point the camera to the center of the scene
-        camera.position.x = -20;
-        camera.position.y = 20;
-        camera.position.z = 20;
-        camera.lookAt(scene.position);
+        var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+        var xMiddle = Math.floor(yos[0].length / 2);
+        var yMiddle = Math.floor(yos.length / 2);
+        camera.position.x = xMiddle;
+        camera.position.y = -20; // End of valley
+        camera.position.z = 50;
+        camera.lookAt(new THREE.Vector3(xMiddle, 150, 0));
 
-        // add subtle ambient lighting
-        // var ambientLight = new THREE.AmbientLight(0x494949);
-        // scene.add(ambientLight);
-
-        // add spotlight for the shadows
         var spotLight = new THREE.SpotLight(0xffffff);
-        spotLight.position.set(-20, 20, 20);
+        spotLight.position.set(-100, yMiddle, 100);
         spotLight.castShadow = true;
         scene.add(spotLight);
 
-        // add the output of the renderer to the html element
         this._el.appendChild(renderer.domElement);
 
-        // call the render function
-        var step = 0;
-
         var vertices = [],
-            faces = [],
-            n = 101; // Number of vertices on side
+            faces = [];
 
-        for (var x = 0; x < n; ++x) {
-            for (var y = 0; y < n; ++y) {
-                vertices.push(new THREE.Vector3(x - Math.ceil(n / 2), y - Math.ceil(n / 2), Math.random() * 2));
-                
+        var lng_n = yos.length,
+            lat_n,
+            z;
+
+        for (var x = 0; x < yos.length; ++x) {
+            lat_n = yos[x].length;
+            for (var y = 0; y < lat_n; ++y) {
+                lat_n = yos[x].length;
+                z = 0 === y ? yos[x][y] : z + yos[x][y];
+                vertices.push(new THREE.Vector3(x, y, z / 100)); // Math.random() * 2;
+            
                 if (x && y) {
-                    // Create sides for square for which this vertex is the upper right corner
-                    let ur = vertices.length - 1, // upper right
-                        ul = ur - 1, // upper left
-                        lr = ur - n, // lower right
-                        ll = lr - 1; // lower left
-
-                    // Counter-clockwise
-                    faces.push(new THREE.Face3(ll, lr, ur));
-                    faces.push(new THREE.Face3(ll, ur, ul));
+                    // Create two triangular sides for a square in which 
+                    // this vertex is the upper right corner
+                    let ur = vertices.length - 1,   // upper right
+                        ul = ur - 1,                // upper left
+                        lr = ur - lat_n,            // lower right
+                        ll = lr - 1;                // lower left
 
                     // Clockwise
                     faces.push(new THREE.Face3(ur, lr, ll));
@@ -75,13 +66,10 @@ const Component = React.createClass({
         geom.computeFaceNormals();
 
         var materials = [
-            new THREE.MeshLambertMaterial({ opacity: 0.6, color: 0x444444, transparent: true }),
-            new THREE.MeshBasicMaterial({ color: 0x000000, wireframe: true })
+            new THREE.MeshLambertMaterial({ opacity: 0.6, color: 0x444444 })
         ];
 
         var mesh = THREE.SceneUtils.createMultiMaterialObject(geom, materials);
-        mesh.children.forEach((e) => e.castShadow = true);
-
         scene.add(mesh);
 
         render();
